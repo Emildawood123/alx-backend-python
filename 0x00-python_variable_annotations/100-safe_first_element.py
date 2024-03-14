2e38c581e9ae28375d95f7e29bd41318150a2b71
Emildawood123 <edawood997@gmail.com>
2024-03-14 07:54:22 -0400

    first advanced

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """safe_first_element function"""
-from typing import Union, Sequence, Any, NoneType
+from typing import Union, Sequence, Any
 
 
 # The types of the elements of the input are not know
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """safe_first_element function"""
     if lst:
         return lst[0]
