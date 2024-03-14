7628796e4579fa3baecb8d3053ef389a1aec7127
Emildawood123 <edawood997@gmail.com>
2024-03-14 07:17:49 -0400

    7

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""sum_mixed_list function"""
+from typing import List, Union
+
+Num = Union[int, float]
+
+
+def sum_mixed_list(mxd_lst: List[Num]) -> float:
+    """sum_mixed_list function"""
+    res: float = 0.00
+    for i in input_list:
+        res = res + i
+    return res
