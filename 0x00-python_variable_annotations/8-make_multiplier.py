d27a144666a67ee7f90af77d100b655ec8df1073
Emildawood123 <edawood997@gmail.com>
2024-03-14 07:39:55 -0400

    :

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""make_multiplier function"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """make_multiplier"""
+    return lambda x: x * multiplier
