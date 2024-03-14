b95e83bac8554429fe20802090351acac72da2fb
Emildawood123 <edawood997@gmail.com>
2024-03-14 07:30:50 -0400

    Tuuuuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """sum_mixed_list function"""
-from typing import Union
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """to_kv function"""
     return (k, v)
