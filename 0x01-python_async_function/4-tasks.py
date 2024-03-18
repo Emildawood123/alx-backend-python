09730b9888618ad5dd9fb774cf7861092625cb62
Emildawood123 <edawood997@gmail.com>
2024-03-18 06:40:45 -0400

    last

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,7 +7,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Tasks function"""
     arr: List[float] = []
     for i in range(0, n):
