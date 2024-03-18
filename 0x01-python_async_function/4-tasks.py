abf121787b109ebba7f8418a4fc3d1461f972fe6
Emildawood123 <edawood997@gmail.com>
2024-03-18 06:39:40 -0400

    adddd

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Tasks function"""
+import asyncio
+import random
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Tasks function"""
+    arr: List[float] = []
+    for i in range(0, n):
+        val = wait_random(max_delay)
+        arr.append(await val)
+    return arr
