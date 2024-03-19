d89a09f9c811e2d53683f50a8fa339c30f2c4852
Emildawood123 <edawood997@gmail.com>
2024-03-19 06:12:06 -0400

    edit second

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+"""async_comperhension function"""
 import asyncio
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """async_comperhension function"""
     res = []
     async for i in async_generator():
         res.append(i)
