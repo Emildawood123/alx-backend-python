b9f42dcc213d1a464716d161f8c0a9fea0d505fa
Emildawood123 <edawood997@gmail.com>
2024-04-05 08:10:48 -0400

    forget add property

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,6 +4,7 @@ import utils
 from parameterized import parameterized
 import unittest
 from unittest.mock import Mock, patch
+import functools
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -45,3 +46,26 @@ class TestGetJson(unittest.TestCase):
         mock_get.return_value.json = jsonMock
         self.assertEqual(utils.get_json(test_url), test_payload)
         mock_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """Test Memoize class"""
+
+    def test_memoize(self):
+        """Test memoize"""
+
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @utils.memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method") as mock_a:
+            mock_a.return_value = 10
+            instance = TestClass()
+            result = instance.a_property
+            self.assertEqual(instance.a_property, result)
+            mock_a.assert_called_once()
